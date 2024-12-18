fd247635eb53f05a4ffe36d8df1de2bb8ec757a3
AurinaL-A <irina.shapochkina5432@gmail.com>
2024-12-19 02:36:50 +0500

    Второй модуль окончательно

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false])
 
-<textarea @disabled($disabled) {{$attributes->merge(['class'=> ' focus:border-indigo-500 rounded-md shadow-sm'])}}>
+<textarea @disabled($disabled) {{$attributes->merge(['class'=> 'border-blue-500 focus:border-blue-500 rounded-md shadow-sm'])}}>
     {{$value ?? $slot}}
 </textarea>
--- resources/views/report/create.blade.php
+++ resources/views/report/create.blade.php
@@ -8,28 +8,28 @@
 @include('layouts.flash-messages')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
             <form class="p-4 md:p-5 space-y-4" method="POST" action="{{route('reports.store')}}">
                 @csrf
                 <div class="flex flex-col">
                     <!-- Number -->
                     <div>
-                        <x-input-label for="number" :value="__('Номер автомобиля')" />
-                        <x-text-input id="number" name="number" type="text"  class="block mt-1 py-2  " required />
+                        <x-input-label for="number" :value="__('Номер автомобиля')"  />
+                        <x-text-input id="number" name="number" type="text"  class="block mt-1 py-2  " :placeholder="__('Регистрационный номер авто')" required />
                         <x-input-error :messages="$errors->get('number')" class="mt-2"/>
                     </div>
                     <!-- Desription -->
                     <div>
-                        <x-input-label for="number" :value="__('Описание')" />
-                        <x-textarea id="description" class="block mt-1 py-2" rows="10" cols="35" name="description"/>
+                        <x-input-label for="number" :value="__('Описание')"/>
+                        <x-textarea id="description" class="block mt-1 py-2" rows="10" cols="35" name="description" :placeholder="__('Описание')"/>
                         <x-input-error :messages="$errors->get('number')" class="mt-2"/>
                     </div>
                     <!-- Status -->
 
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{{ __('Создать')}}</button>
+                <button type="submit" class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{{ __('Создать заявление')}}</button>
             </form>
-        </div>
+        
         </div>
         
 
